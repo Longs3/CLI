76e80ddd115f075ad33730c49b0da960aeec35e9
Longs3 <senglong0887663213@gmail.com>
2022-10-03 10:15:09 +0700

    changes

--- JS/Javascript/for_header.js
+++ JS/Javascript/for_header.js
@@ -1,10 +1,11 @@
 function cat(val1, val2) { return val1 + val2; }
+function clear() { console.clear(); }
 function cp(val1, val2) { val1 = val2; return val1; }
-function factorial(val1) { return val1 > 1 ? val1 * Factorial(val1 - 1) : 1; }
+function factorial(val1) { return val1 > 1 ? val1 * factorial(val1 - 1) : 1; }
 function len(val1) { return val1.length; }
 function reverseString(str) { return [...String(str)].reverse().join(''); }
-function clear() { console.clear(); }
-
+function toLower(val1) { return val1.toLowerCase(); }
+function toUpper(val1) { return val1.toUpperCase(); }
 // bit
 function decimalToBinary(val1) { return Number.isNaN(val1) ? 0 : val1.toString(2); }
 function decimalToOctal(val1) { return Number.isNaN(val1) ? 0 : val1.toString(8); }
@@ -16,6 +17,12 @@ function octalToDecimal(val1) { return Number.isNaN(val1) ? 0 : parseInt(val1, 8
 function octalToBinary(val1) { return Number.isNaN(val1) ? 0 : decimalToBinary(parseInt(val1, 8)); }
 function octalToBinary(val1) { return Number.isNaN(val1) ? 0 : decimalToHexa(parseInt(val1, 8)); }
 function hexaToDecimal(val1) { return Number.isNaN(val1) ? 0 : parseInt(val1, 16); }
+function haxaTodecimal(val1) {
+    let [i, f] = val1.split('.');
+    i = parseInt(i, 16);
+    f = parseInt(f, 16) / Math.pow(16, f.length);
+    return i + f;
+}
 // temperature
 function celsiusToKelvin(val1) { return (Number.isNaN(val1)) ? 0 : val1 + 273; }
 function celsiusToFaren(val1) { return (Number.isNaN(val1)) ? 0 : (9 / 5) * val1 + 32; }
--- JS/Javascript/test.js
+++ JS/Javascript/test.js
@@ -1,6 +1,4 @@
-
-function dow(a, b) {return (a>b)? 1:0 ;}
-console.log(dow(2,1));
-console.log(String.raw`d'she`);
-var ste = '           thehee            ';
-console.log(ste.trim());
+var img = document.createElement('img');
+img.src='../../image/1.jpg';
+img.style.width = '200px';
+document.body.appendChild(img);
